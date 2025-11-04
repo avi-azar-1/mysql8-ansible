500d43e9c4c2e1ee7e827312024982d61405fd8d
avi-azar-1 <addeeff147@gmail.com>
2025-11-04 20:49:24 +0200

    Add files via upload

---
+++ add_module.sh
@@ -0,0 +1,4 @@
+mkdir -p /root/.ansible/collections/ansible_collections/community/mysql
+cp community-mysql-4.0.1.tar.gz /root/.ansible/collections/ansible_collections/community/mysql
+cd /root/.ansible/collections/ansible_collections/community/mysql
+tar -xzf community-mysql-4.0.1.tar.gz
